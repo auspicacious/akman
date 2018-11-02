84bd90853570ba353297a5cdc8759643f5ee64da
Andrew Todd <at@auspicacious.org>
2018-11-02 16:11:29 +0900

    Removing some experimental classes.

--- akman-lib/src/main/java/org/auspicacious/akman/lib/interfaces/SignatureVerifier.java
+++
@@ -1,8 +0,0 @@
-package org.auspicacious.akman.lib.interfaces;
-
-import org.auspicacious.akman.lib.impl.SignatureVerifierException;
-
-public interface SignatureVerifier {
-    public boolean verifyCertificate();
-    public boolean verifyCMS();
-}
